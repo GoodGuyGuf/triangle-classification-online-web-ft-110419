38dd3d876a26a2ac38367cc1fa9f8a1f578c158e
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-29 00:32:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
+require 'pry'
 class Triangle
-  # write code here
+
+
+  def initialize(side_1, side_2, side_3)
+    @sides = []
+    @sides << side_1
+    @sides << side_2
+    @sides << side_3
+  end
+
+  def valid?
+  sum = @sides[0] + @sides[1]
+  sum_2 = @sides[1] + @sides[2]
+    if sum < @sides[2] ||
+      sum <= 0 ||
+      @sides[2] < 0 ||
+      @sides[0] == @sides[2] && @sides[1] > @sides[2] ||
+      sum_2 < @sides[0]
+      false
+    else
+      true
+    end
+  end
+
+  def kind
+    if valid?
+      if @sides.uniq.length == 1
+        return :equilateral
+      elsif @sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+    puts "Ya need to research ya maths!"
+    end
+  end
 end
